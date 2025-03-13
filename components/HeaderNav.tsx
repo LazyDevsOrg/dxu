794f36b3e505b60b0844e787a74757e86898afcf
Kd <86334894+kdlanguido@users.noreply.github.com>
2025-03-13 22:53:27 +0800

    Added MUI -JOY

---
+++ components/HeaderNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { InputType } from 'node:zlib'
+import { Typography } from '@mui/joy'
+
+export default function HeaderNav() {
+    return (
+        <div className="!bg-[#3E1717] !h-[10vh] !text-[#fff]">
+            <Typography>DXU</Typography>
+        </div>
+    )
+}
