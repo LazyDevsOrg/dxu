500ebaf564f10677a97bcafa0a876830200ddeed
Kd <86334894+kdlanguido@users.noreply.github.com>
2025-03-13 23:06:54 +0800

    Added Header

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { InputType } from 'node:zlib'
-import { Typography } from '@mui/joy'
+import { Link, Typography } from '@mui/joy'
+import { NavLinks } from '@/constants/app'
 
 export default function HeaderNav() {
     return (
-        <div className="!bg-[#3E1717] !h-[10vh] !text-[#fff]">
-            <Typography>DXU</Typography>
+        <div className="!bg-[#3E1717] !h-[10vh] flex justify-between items-center p-5">
+
+            <div className="flex flex-row gap-10 items-center">
+                <Link className="!text-[#fff] !font-bold !text-[40px]" href="/">DXU</Link>
+            </div>
+
+            <div className="flex flex-row gap-6 items-center">
+                {NavLinks.map((nav, index) => (
+                    <Link key={index} className="!text-[#fff] !font-light !text-[20px]" href={nav.url}>{nav.name}</Link>
+                ))}
+            </div>
+
+            <div className="flex flex-row gap-3 items-center !text-[#fff]">
+                <Link className="!text-[#fff] !font-light !text-[20px]" href="/login">Login</Link>
+                |
+                <Link className="!text-[#fff] !font-light !text-[20px]" href="/register">Register</Link>
+            </div>
+
         </div>
     )
 }
---
+++ constants/app.ts
@@ -0,0 +1,18 @@
+export const NavLinks = [
+    {
+        name: "Events",
+        url: "/events",
+    }, {
+        name: "Members",
+        url: "/members",
+    }, {
+        name: "Certificates",
+        url: "/certificates",
+    }, {
+        name: "Mission & Vision",
+        url: "/mission-vision",
+    }, {
+        name: "Pledge",
+        url: "/pledge",
+    },
+]
